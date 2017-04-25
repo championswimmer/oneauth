a206c04321ff318130e9bcad535650ea647cf531
Arnav Gupta <arnav@codingblocks.com>
2017-04-25 17:34:12 +0530

    empty clients handler
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- routers/pagerouter.js
+++ routers/pagerouter.js
@@ -6,6 +6,12 @@ const publicroute = require('./pages/public')
     , userroute = require('./pages/user')
     , clientroute = require('./pages/client');
 
+router.use(function (req, res, next) {
+    // One '!' doesn't cancel the other'!'. This is not wrong code. Learn JS
+    res.locals.loggedIn = !!req.user;
+    next();
+});
+
 router.use('/', publicroute);
 router.use('/users', userroute);
 router.use('/clients', clientroute);
--- routers/pages/public.js
+++ routers/pages/public.js
@@ -6,15 +6,16 @@ const cel = require('connect-ensure-login');
 const router = require('express').Router();
 
 router.get('/login', function(req, res, next) {
-    res.render('login', {title: "Login | OneAuth", loggedIn : "hidden"})
+    res.render('login', {title: "Login | OneAuth"})
 });
 router.get('/signup', function(req, res, next) {
-    res.render('signup', {title: "Signup | OneAuth", loggedIn : "hidden"})
+    res.render('signup', {title: "Signup | OneAuth"})
 });
+
 router.get('/client/add',
     cel.ensureLoggedIn('/login'),
     function(req, res, next) {
-        res.render('client/add', {title: "Add New Client | OneAuth", loggedIn : "visible"})
+        res.render('client/add', {title: "Add New Client | OneAuth"})
     }
 );
 

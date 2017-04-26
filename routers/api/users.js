9a5edece0f9fd3451d1a53ecfc3946aa2c879b9f
Arnav Gupta <arnav@codingblocks.com>
2017-04-27 05:13:14 +0530

    yoohoo, lms is ours
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- routers/api/users.js
+++ routers/api/users.js
@@ -12,13 +12,39 @@ const models = require('../../db/models').models;
 router.get('/me',
     passport.authenticate('bearer', {session: false}),
     function(req,res) {
-       if (req.user) {
-           res.send(req.user)
-       } else {
-           res.sendStatus(403)
-       }
-    }
-);
+
+        if (req.user) {
+            let includes = [];
+            if (req.query.include) {
+                let includedAccounts = req.query.include.split(',');
+                for (ia of includedAccounts) {
+                    switch (ia) {
+                        case 'facebook': includes.push(models.UserFacebook); break;
+                        case 'twitter': includes.push(models.UserTwitter); break;
+                        case 'github': includes.push(models.UserGithub); break;
+                        case 'lms': includes.push(models.UserLms); break;
+                    }
+                }
+            }
+
+
+            models.User.findOne({
+                where: {id: req.user.id},
+                include: includes
+            }).then(function (user) {
+                if (!user) {
+                    throw err;
+                }
+                res.send(user);
+            }).catch(function(err) {
+                res.send('Unknown user or unauthorized request');
+            })
+
+        } else {
+            return res.sendStatus(403)
+        }
+
+    });
 
 router.get('/:id',
     passport.authenticate('bearer', {session: false}),

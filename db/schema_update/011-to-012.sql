3046d92895ba5434b8bafc2e69ca3631250ee05f
Arnav Gupta <arnav@codingblocks.com>
2017-08-25 01:38:36 +0000

    fix schema
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- db/schema_update/011-to-012.sql
+++ db/schema_update/011-to-012.sql
@@ -1,3 +1,2 @@
 ﻿﻿CREATE TYPE public.enum_users_role AS ENUM ('employee', 'intern', 'admin');
-ALTER TYPE public.enum_users_role OWNER TO postgres;
 ALTER TABLE public.users ADD COLUMN role enum_users_role;

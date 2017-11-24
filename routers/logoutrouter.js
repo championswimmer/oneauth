a8f7e71052cbd18026bac220c58b9438b9874cb5
Abhishek Gupta <hi@itsabhishek.me>
2017-11-24 14:54:48 +0530

    allow redirect url after login

--- routers/logoutrouter.js
+++ routers/logoutrouter.js
@@ -2,12 +2,17 @@
  * Created by piyush0 on 23/04/17.
  */
 const router = require('express').Router();
+const Raven = require('raven');
 
 router.get('/', function (req, res){
+	const redirectUrl = req.query.redirect || '/'
     req.user = null;
     req.logout();
     req.session.destroy(function (err) {
-        res.redirect('/');
+    	if (err) 
+    		Raven.captureException(err)
+    	
+        res.redirect(redirectUrl);
     });
 });
 

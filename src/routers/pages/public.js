43368b9e1d22f930e6d595cc5b32e40e2fb70716
Arnav Gupta <arnav@codingblocks.com>
2018-02-09 14:36:19 +0530

    refactor the setnew password route completely
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/routers/pages/public.js
+++ src/routers/pages/public.js
@@ -1,5 +1,7 @@
 /**
  * Created by championswimmer on 13/03/17.
+ *
+ * This route contains pages that are visible to public (without logging in)
  */
 const cel = require('connect-ensure-login')
 
@@ -7,21 +9,15 @@ const router = require('express').Router()
 
 const verifyemail = require('../../routers/verifyemail')
 
-const notForLoggedIn = (req, res, next) => {
-    if (req.user) {
-        return res.redirect('/')
-    }
-    next()
-}
 
-router.get('/login', notForLoggedIn, function (req, res, next) {
+router.get('/login', cel.ensureNotLoggedIn('/'), function (req, res, next) {
     res.render('login', {title: "Login | OneAuth", error: req.flash('error')})
 })
-router.get('/signup', notForLoggedIn, function (req, res, next) {
+router.get('/signup', cel.ensureNotLoggedIn('/'), function (req, res, next) {
     res.render('signup', {title: "Signup | OneAuth"})
 })
 
-router.get('/setnewpassword/:key', notForLoggedIn, function (req, res, next) {
+router.get('/forgot/password/new/:key', cel.ensureNotLoggedIn('/'), function (req, res, next) {
     res.render('forgot/password/new', {title: "Setnewpassword | OneAuth", key: req.params.key})
 })
 

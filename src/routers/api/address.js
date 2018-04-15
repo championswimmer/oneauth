c8b23e1083d8ab9dadd862f325f2d62ed3cf60d0
Arnav Gupta <arnav@codingblocks.com>
2018-04-16 01:52:11 +0530

    wrap into transaction
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/routers/api/address.js
+++ src/routers/api/address.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const {db, models} = require('../../db/models')
-const embed = require('sequelize-embed')(db)
 const generator = require('../../utils/generator')
 const cel = require('connect-ensure-login')
 const Raven = require('raven')
@@ -44,34 +43,38 @@ router.post('/:id', cel.ensureLoggedIn('/login'), async function (req, res) {
     let addrId = parseInt(req.params.id)
     let userId = parseInt(req.user.id)
 
+
     try {
-        if (req.body.primary === 'on') {
+        await db.transaction(async (t) => {
+            if (req.body.primary === 'on') {
 
-            let demo = await models.Demographic.findOne({userId: req.user.id})
-            let demoId = demo.id
-            await models.Address.update(
-                {primary: false},
-                {where: {demographicId: demoId}}
+                let demo = await models.Demographic.findOne({where: {userId: req.user.id}})
+                let demoId = demo.id
+                await models.Address.update(
+                    {primary: false},
+                    {where: {demographicId: demoId}}
+                )
+            }
+
+            await models.Address.update({
+                    label: req.body.label,
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    mobile_number: req.body.number,
+                    email: req.body.email,
+                    pincode: req.body.pincode,
+                    street_address: req.body.street_address,
+                    landmark: req.body.landmark,
+                    city: req.body.city,
+                    stateId: req.body.stateId,
+                    countryId: req.body.countryId,
+                    primary: req.body.primary === 'on'
+                },
+                { where: {id: addrId} }
             )
-        }
+            return res.redirect(`/address/${addrId}`)
+        })
 
-        await models.Address.update({
-                label: req.body.label,
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                mobile_number: req.body.number,
-                email: req.body.email,
-                pincode: req.body.pincode,
-                street_address: req.body.street_address,
-                landmark: req.body.landmark,
-                city: req.body.city,
-                stateId: req.body.stateId,
-                countryId: req.body.countryId,
-                primary: req.body.primary === 'on'
-            },
-            { where: {id: addrId} }
-        )
-        return res.redirect(`/address/${addrId}`)
     } catch (err) {
         Raven.captureException(err)
         req.flash('error', 'Could not update address')

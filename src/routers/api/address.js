281f6d2c08e046f7c5a8f65ef0235a16e3d8add4
Arnav Gupta <arnav@codingblocks.com>
2018-04-16 01:47:08 +0530

    address update works
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/routers/api/address.js
+++ src/routers/api/address.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
-const models = require('../../db/models').models;
-const generator = require('../../utils/generator');
-const cel = require('connect-ensure-login');
-const Raven = require('raven');
-const urlutils = require('../../utils/urlutils');
-const { hasNull } = require('../../utils/nullCheck');
+const router = require('express').Router()
+const {db, models} = require('../../db/models')
+const embed = require('sequelize-embed')(db)
+const generator = require('../../utils/generator')
+const cel = require('connect-ensure-login')
+const Raven = require('raven')
+const urlutils = require('../../utils/urlutils')
+const {hasNull} = require('../../utils/nullCheck')
 
 router.post('/', cel.ensureLoggedIn('/login'), function (req, res) {
-    if(hasNull(req.body, ['label','first_name','last_name','number','email','pincode','street_address','landmark','city','stateId','countryId'])) {
-        res.send(400);
+    if (hasNull(req.body, ['label', 'first_name', 'last_name', 'number', 'email', 'pincode', 'street_address', 'landmark', 'city', 'stateId', 'countryId'])) {
+        res.send(400)
     } else {
         models.Demographic.findCreateFind({
             where: {userId: req.user.id}
@@ -34,18 +35,27 @@ router.post('/', cel.ensureLoggedIn('/login'), function (req, res) {
                 res.redirect('/users/me')
             })
     }
-});
+})
 
-router.post('/:id', cel.ensureLoggedIn('/login'), function (req, res) {
-    if(hasNull(req.body, ['label','first_name','last_name','number','email','pincode','street_address','landmark','city','stateId','countryId'])) {
-        return res.send(400);
+router.post('/:id', cel.ensureLoggedIn('/login'), async function (req, res) {
+    if (hasNull(req.body, ['label', 'first_name', 'last_name', 'number', 'email', 'pincode', 'street_address', 'landmark', 'city', 'stateId', 'countryId'])) {
+        return res.send(400)
     }
-    let id = parseInt(req.params.id);
-    if (req.body.primary === 'on') {
-        models.Address.update({
-            primary:false
-        },{where: {userId:req.user.id}}).then( _ => {
-            return models.Address.update({
+    let addrId = parseInt(req.params.id)
+    let userId = parseInt(req.user.id)
+
+    try {
+        if (req.body.primary === 'on') {
+
+            let demo = await models.Demographic.findOne({userId: req.user.id})
+            let demoId = demo.id
+            await models.Address.update(
+                {primary: false},
+                {where: {demographicId: demoId}}
+            )
+        }
+
+        await models.Address.update({
                 label: req.body.label,
                 first_name: req.body.first_name,
                 last_name: req.body.last_name,
@@ -57,40 +67,18 @@ router.post('/:id', cel.ensureLoggedIn('/login'), function (req, res) {
                 city: req.body.city,
                 stateId: req.body.stateId,
                 countryId: req.body.countryId,
-                userId: req.user.id,
                 primary: req.body.primary === 'on'
-            }, {
-                where: {id: id}
-            });
-        }).then(function (address) {
-            res.redirect('/address/' + id)
-        }).catch(function (error) {
-            Raven.captureException(error)
-        })
-    } else {
-        models.Address.update({
-            label: req.body.label,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            mobile_number: req.body.number,
-            email: req.body.email,
-            pincode: req.body.pincode,
-            street_address: req.body.street_address,
-            landmark: req.body.landmark,
-            city: req.body.city,
-            stateId: req.body.stateId,
-            countryId: req.body.countryId,
-            userId: req.user.id,
-            primary: req.body.primary === 'on'
-        }, {
-            where: {id: id}
-        }).then(function (address) {
-            res.redirect('/address/' + id)
-        }).catch(function (error) {
-            Raven.captureException(error)
-        })
+            },
+            { where: {id: addrId} }
+        )
+        return res.redirect(`/address/${addrId}`)
+    } catch (err) {
+        Raven.captureException(err)
+        req.flash('error', 'Could not update address')
+        res.redirect('/address')
     }
-});
+
+})
 
 
 module.exports = router

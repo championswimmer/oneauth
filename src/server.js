b57008564d452350ae37563ecbfbd8f381ffa382
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-05-23 15:59:24 +0530

    Made the changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 /**
  * Created by championswimmer on 08/03/17.
  */
-require('newrelic')
 const express = require('express')
     , bodyParser = require('body-parser')
     , session = require('express-session')

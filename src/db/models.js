eb3f0f9da1002e7ca60dceb599795273664fdaca
Arnav Gupta <arnav@codingblocks.com>
2018-06-08 15:35:54 +0530

    make users paranoid delete
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/db/models.js
+++ src/db/models.js
@@ -36,6 +36,8 @@ const User = db.define('user', {
     email: Sequelize.DataTypes.STRING,
     role: {type: Sequelize.DataTypes.ENUM('admin', 'employee', 'intern'), allowNull: true},
     verifiedemail: {type: Sequelize.DataTypes.STRING, defaultValue: null, unique: true, allowNull: true}
+}, {
+    paranoid: true
 })
 
 const Resetpassword = db.define('resetpassword', {

a42b3d12501d939917408d71db503c7f6247dcfd
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-07-14 17:38:42 +0530

    issue fixed

--- src/routers/pages/user.js
+++ src/routers/pages/user.js
@@ -142,7 +142,13 @@ router.post('/me/edit',
             if (req.body.collegeId) {
                 demographic.collegeId = +req.body.collegeId
             }
-            await models.Demographic.upsert({userId:demographic.userId,collegeId:demographic.collegeId,branchId:demographic.branchId})
+
+            let userDemographic = await models.Demographic.findOne({
+                where:{
+                    userId:demographic.userId
+                }
+            })
+            await models.Demographic.upsert({id:userDemographic.id,collegeId:demographic.collegeId,branchId:demographic.branchId})
 
             if (req.body.password) {
                 const passHash = await passutils.pass2hash(req.body.password)

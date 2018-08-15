eb1243ab857ec44b909180c3557aed999de8a133
VibhorGupta <vibhordelgupta@gmail.com>
2018-08-15 22:21:23 +0530

    linkedin (#335)
    * linkedin
    
    * email
    
    * migration

---
+++ migrations/061-to-062.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.userlinkedins ADD COLUMN email VARCHAR(255);
--- src/passport/strategies/user/strategy-linkedin.js
+++ src/passport/strategies/user/strategy-linkedin.js
@@ -19,6 +19,7 @@ module.exports = new LinkedinStrategy({
 
       let profileJson = profile._json
       let oldUser = req.user
+      profileJson.email = profileJson.emails[0].value
       Raven.setContext({extra: {file: 'linkedinStrategy'}})
 
       try{
@@ -35,10 +36,11 @@ module.exports = new LinkedinStrategy({
                   throw new Error('Your Linkedin account is already linked with codingblocks account Id: ' + lkaccount.dataValues.userId)
               } else {
                   const updated = await models.UserLinkedin.upsert({
-                    id: profileJson.id,
+                      id: profileJson.id,
                       token: token,
                       tokenSecret: tokenSecret,
                       username: profileJson.formattedName,
+                      email: profileJson.email,
                       profile: profileJson.publicProfileUrl,
                       userId: oldUser.id
                   })

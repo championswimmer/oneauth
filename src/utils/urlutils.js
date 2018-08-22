8b0d5b460f8fd71ba3d92e4eaf2ac87de0ca1124
Arnav Gupta <arnav@codingblocks.com>
2018-08-23 03:52:09 +0530

    improve url utils
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/utils/urlutils.js
+++ src/utils/urlutils.js
@@ -4,14 +4,14 @@
 
 module.exports = {
     prefixHttp: function (url) {
-        if (url.substr(0, 4) != "http") {
+        if (url.substr(0, 4) !== "http") {
             url = "http://" + url
         }
         return url
     },
 
     isURL: function (url) {
-        const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
-        return regex.test(url) ? true : false
+        const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
+        return regex.test(url)
     }
 }

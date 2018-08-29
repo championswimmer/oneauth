ce7639f190d862c5be9290f3c89f42aaea6240ef
Arnav Gupta <arnav@codingblocks.com>
2018-08-29 18:36:53 +0530

    reformat: fix code syntax on profile validation middleware
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/middlewares/profilevalidation.js
+++ src/middlewares/profilevalidation.js
@@ -1,5 +1,9 @@
+
 function redirectToEditProfile(req, res, next) {
-  if (req.path == "/users/me/edit") return next();
+
+  if (req.path === "/users/me/edit") {
+      return next();
+  }
   if (req.user && (!req.user.email || !req.user.mobile_number)) {
     return res.redirect("/users/me/edit");
   }

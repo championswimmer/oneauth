7a5637c1fef647efcfd2a83492801cee40177b14
Arnav Gupta <arnav@codingblocks.com>
2018-08-29 18:40:22 +0530

    feature: allow login with verified email id
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/passport/strategies/user/strategy-local.js
+++ src/passport/strategies/user/strategy-local.js
@@ -2,6 +2,7 @@
  * Created by championswimmer on 07/05/17.
  */
 const Raven = require('raven')
+const {Op} = require('sequelize')
 const LocalStrategy = require('passport-local').Strategy
 const models = require('../../../db/models').models
 
@@ -16,12 +17,22 @@ const passutils = require('../../../utils/password')
 module.exports = new LocalStrategy(async function (username, password, cb) {
 
     Raven.setContext({extra: {file: 'localstrategy'}})
-    try{
+    try {
         const userLocal = await models.UserLocal.findOne({
-            include: [{model: models.User, where: {username: username}}],
+            include: [
+                {
+                    model: models.User,
+                    where: {
+                        [Op.or]: [
+                            {username: username},
+                            {verifiedemail: username} // allow login via verified email too
+                        ]
+                    }
+                }
+            ],
         })
         if (!userLocal) {
-            return cb(null, false, {message: 'Invalid Username'})
+            return cb(null, false, {message: 'Invalid Username or Unverified Email'})
         }
 
         const match = await passutils.compare2hash(password, userLocal.password)
@@ -31,8 +42,8 @@ module.exports = new LocalStrategy(async function (username, password, cb) {
             return cb(null, false, {message: 'Invalid Password'})
         }
 
-    }catch(err) {
+    } catch (err) {
         Raven.captureException(err)
-        return cb(null, false, {message: 'Could not create account'})
+        return cb(null, false, {message: 'Error connecting to user database'})
     }
 })
